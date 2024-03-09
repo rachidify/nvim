ebc81227f1a33ab308c754921cdc41a9f59b424a
Rasheed Hamdawi <rasheedhamdawi@gmail.com>
2024-03-09 18:23:29 +0800

    add onedark theme

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -1,11 +1,18 @@
 -- Plugin management (using packer.nvim)
 local packer_exists = pcall(vim.cmd, [[packadd packer.nvim]])
 if not packer_exists then
-    vim.api.nvim_command('!git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim')
-    vim.api.nvim_command('packadd packer.nvim')
+  vim.api.nvim_command(
+    '!git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim')
+  vim.api.nvim_command('packadd packer.nvim')
 end
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
-end)
 
+  use {
+    'navarasu/onedark.nvim',
+    config = function()
+      require('onedark').load()
+    end
+  }
+end)
