147ef72291e03c71624049d7c69c9925d8667410
Rasheed Hamdawi <rasheedhamdawi@gmail.com>
2024-03-09 18:30:58 +0800

    add statusline

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -22,4 +22,12 @@ return require('packer').startup(function(use)
       require('nvim-tree').setup()
     end
   }
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+    config = function()
+      require('lualine').setup()
+    end
+  }
 end)
